45f3e748925ca1bc5446b5fff46b97e8470b012e
jessica <jessica_02_06@outlook.com>
2022-09-25 22:49:09 -0300

    criando rede sociaL

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,1 +1,36 @@
-/* Desenvolva a lógica da página aqui */
+let buttonFollw = document.querySelectorAll(".bt_follow")
+
+for(let i = 0; i < buttonFollw.length; i++){
+    buttonFollw[i].addEventListener("click", function(){
+        let button = buttonFollw[i]
+        let trocaStyle = button.classList.toggle("bt_following")
+        if(trocaStyle){
+            button.textContent = "Seguindo"
+        }else{
+            button.textContent = "Seguir"
+        }
+        
+    })
+    
+}
+
+
+const buttonModal = document.querySelectorAll("[data-modal]")
+
+for(let i = 0; i < buttonModal.length; i++){
+    buttonModal[i].addEventListener("click", function(){
+        let valueButtonModal = buttonModal[i].getAttribute("data-modal")
+
+        document.getElementById(valueButtonModal).classList.toggle("show_screen")
+        console.log(document.getElementById(valueButtonModal))
+    })
+}
+
+let button_heart = document.querySelectorAll(".bt_heart")
+
+for (let i = 0; i < button_heart.length; i++){
+    button_heart[i].addEventListener("click", function(){
+        let button = button_heart[i]
+        button.innerHTML = "❤️"  
+})
+}
